f625f9ba0b3c9bb2c042e3bb5d88869581dcb233
hamz-zzz <hamzamahmood2k@gmail.com>
2026-01-08 21:18:32 -0500

    y

---
+++ my_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# This is a comment
+my_ip_addr = input("Enter an IP address: ")
+
+print(my_ip_addr)
+
+print('Hello again') #another comment
+
+"""
+This is a multi
+line comment
+"""
