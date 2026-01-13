da16c9a9ae265f3dd03ac8934b976b86885d7897
hamz-zzz <hamzamahmood2k@gmail.com>
2026-01-13 14:59:47 -0500

    y

--- lesson3/exercise2.py
+++ lesson3/exercise2.py
@@ -1,12 +1,9 @@
 with open('show_ip_int_brief.txt') as f:
-    data = f.readlines()
-
-for x in data:
-    if '10.220' in x:
-        lst = (x.split())
-
-intf_name = lst[0]
-ip_addr = lst[1]
+    for line in f:
+        if '10.220' in line:
+            fields = line.split()
+            intf_name = fields[0]
+            ip_addr = fields[1]
 
 print(f"Interface: {intf_name}")
 print(f"IP Address: {ip_addr}")
